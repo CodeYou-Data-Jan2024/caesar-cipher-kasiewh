6d269aa51e26ecb56aee88bc7677b97f8ca1af16
kasie wallace hall <redheadsforever22@gmail.com>
2024-04-07 20:59:09 -0400

    Code added

--- cipher.py
+++ cipher.py
@@ -1,1 +1,23 @@
-# add your code here
+
+print("Input phrase: ")
+phrase = input()
+
+print("You entered: " + phrase)
+
+alphabet = "abcdefghijklmnopqrstuvwxyzabcde"
+newPhrase = " "
+
+
+
+
+for i in range(0, len(phrase)):
+    try:
+        char = phrase[i]
+        newChar = str(alphabet[alphabet.index(char)+5])
+        newPhrase += newChar
+    except:
+        newChar = str(alphabet[0])
+        newPhrase += newChar
+
+print("The Cipher Phrase is" + newPhrase)
+
